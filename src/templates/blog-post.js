5c66c90d9e93f35afe0831cbc826505990c55eb7
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2019-03-20 20:02:49 -0300

    fix: query description in posts-by-tag view, add tags to blog-post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,16 +4,18 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Tag from "../components/tag"
 import { rhythm, scale } from "../utils/typography"
-import { dateParser } from "../utils/dateParser";
+import { dateParser } from "../utils/dateParser"
 
 import RetomarScrollTooltip from '../components/retomar-scroll-tooltip'
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
-    const postTitle = post.frontmatter.title;
+    const { title: postTitle, tags, description, date } = post.frontmatter;
     const siteTitle = this.props.data.site.siteMetadata.title
+
     const { previous, next } = this.props.pageContext
 
     return (
@@ -21,18 +23,24 @@ class BlogPostTemplate extends React.Component {
         <RetomarScrollTooltip postTitle={postTitle}/>
         <SEO
           title={postTitle}
-          description={post.frontmatter.description || post.excerpt}
+          description={description || post.excerpt}
         />
         <h1>{postTitle}</h1>
+        {
+          tags
+            ? (<div className='tags-container' style={{marginTop: rhythm(-1)}}>
+                {tags.map((tag, i) => <Tag tag={tag} key={i} />)}
+              </div>)
+            : null
+        }
         <p
           style={{
             ...scale(-1 / 5),
             display: `block`,
             marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
           }}
         >
-          {dateParser(post.frontmatter.date)}
+          {dateParser(date)}
         </p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <hr
@@ -94,6 +102,7 @@ export const pageQuery = graphql`
         title
         date(formatString: "DD-MM-YYYY")
         description
+        tags
       }
     }
   }
--- src/templates/posts-by-tag.js
+++ src/templates/posts-by-tag.js
@@ -77,6 +77,7 @@ export const pageQuery = graphql`
               tags
               title
               date
+              description
             }
           }
         }
