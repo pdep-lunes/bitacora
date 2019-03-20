31e958a4fb0f3b5472ce37a625c6baa5c1b6f984
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2019-03-20 01:17:19 -0300

    scroll indicator plugin y hook para recordar scroll

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,5 +75,11 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-scroll-indicator`,
+      options: {
+        color: '#000000',
+      },
+    },
   ],
 }
